4e8b913168e814b3900b1f2f9d53edf8872496dc
gokupraba214 <88436895+gokupraba214@users.noreply.github.com>
2021-09-20 17:55:02 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+ <div class="footer">
+     <div class="container">
+         <div class="strp">
+             <img src="images/ftr.png" alt=""/>
+         </div>
+         <div class="copywrite">
+             <p style="color: white">Copyright © MIX STATION CREW ENTERPRICE </p>
+         </div>
+         
+     </div>
+     </div> 
